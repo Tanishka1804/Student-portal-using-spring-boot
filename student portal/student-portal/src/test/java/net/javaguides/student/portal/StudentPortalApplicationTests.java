d5169105afa3f5142ab5a8170ac9a384837b8a3e
Tanishka1804 <tanishkamodi1823@gmail.com>
2024-11-22 13:33:32 +0530

    Add files via upload

---
+++ student portal/student-portal/src/test/java/net/javaguides/student/portal/StudentPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.student.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
